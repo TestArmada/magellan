a20d8bb2282f6dd00a8d51ed25bfb20d2c0c46b4
Maciej Adwent <MAdwent@walmartlabs.com>
2015-08-14 10:25:24 -0700

    Remove appium details

--- src/interop/lib/mocha_settings.js
+++ src/interop/lib/mocha_settings.js
@@ -4,8 +4,5 @@ var argv = margs.argv;
 module.exports = {
   mochaOpts: argv.mocha_opts, // --mocha_opts opts_file
 
-  mochaTestFolders: argv.mocha_tests, // --mocha_tests location (or array in magellan.json)
-
-  // TODO: deprecate and make this the responsibility of the test suite itself
-  appiumApplicationLocation: argv.appium_application_location
+  mochaTestFolders: argv.mocha_tests // --mocha_tests location (or array in magellan.json)
 };
