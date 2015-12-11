ac3a90dc59b5e28a7e4270cd01746c42190615f1
Maciej Adwent <MAdwent@walmartlabs.com>
2015-12-11 14:42:44 -0800

    add tests and error handling for nightwatch get_tests functionality

--- src/interop/magellan-nightwatch/get_tests.js
+++ src/interop/magellan-nightwatch/get_tests.js
@@ -1,8 +1,17 @@
 var fs = require("fs");
 var path  = require("path");
 var settings = require("../../settings");
+var clc = require("cli-color");
 
 var nightwatchConfigFilePath = settings.nightwatchConfigFilePath;
+
+var filterFiles = function (files) {
+  return files.filter(function (f) {
+    return (f.indexOf(".js") === f.length - 3);
+  }).map(function (f) {
+    return f.trim();
+  });
+};
 
 module.exports = function () {
   var nightwatchConfig;
@@ -11,27 +20,36 @@ module.exports = function () {
     nightwatchConfig = require(path.resolve(nightwatchConfigFilePath));
     console.log("Magellan-nightwatch test iterator found nightwatch configuration at: " + nightwatchConfigFilePath);
   } catch (err) {
-    var error = "Magellan-nightwatch test iterator cannot read nightwatch configuration: " + err.toString();
-    return;
+    throw new Error("Magellan-nightwatch test iterator cannot read nightwatch configuration: " + err.toString());
+  }
+
+  if (!nightwatchConfig.src_folders) {
+    throw new Error("Magellan-nightwatch test iterator cannot find tests: src_folders in nightwatch configuration is missing");
+  }
+
+  if (nightwatchConfig.src_folders.length === 0) {
+    throw new Error("Magellan-nightwatch test iterator cannot find tests: src_folders in nightwatch configuration is present, but empty");
   }
 
   var srcFolders = nightwatchConfig.src_folders;
 
   var allFiles = [];
   srcFolders.forEach(function (folder) {
-    var files = fs.readdirSync(path.normalize(folder));
+    var p = path.normalize(folder);
+    console.log("Scanning " + p + " for test files ...");
+    var files = fs.readdirSync(p);
+
     if (files.length > 0) {
-      allFiles = allFiles.concat(files.map(function (f) {
+      var additionalFiles = filterFiles(files.map(function (f) {
         return path.normalize(folder) + "/" + f;
       }));
+      if (additionalFiles.length > 0 ) {
+        console.log(clc.green("Found " + additionalFiles.length + " test files in " + p));
+      } else {
+        console.log(clc.yellow("Found no test files in " + p));
+      }
+      allFiles = allFiles.concat(additionalFiles);
     }
-  });
-
-  // Ensure we scan for JS only, ignoring README files, etc.
-  allFiles = allFiles.filter(function (f) {
-    return (f.indexOf(".js") === f.length - 3);
-  }).map(function (f) {
-    return f.trim();
   });
 
   return allFiles;
