0dde9c0be0f206cfd55fb308e9b08388f0edc04e
Maciej Adwent <maciekadwent@gmail.com>
2016-01-13 09:25:33 -0800

    linting

--- src/reporters/admiral/settings.js
+++ src/reporters/admiral/settings.js
@@ -1,15 +1,17 @@
+"use strict";
+
 // Configure Admiral from Jenkins or user set environment variables.
 
 module.exports = {
-  enabled:        !!process.env.ADMIRAL_AUTH_ID,
+  enabled: !!process.env.ADMIRAL_AUTH_ID,
 
-  authId:         process.env.ADMIRAL_AUTH_ID,
-  authToken:      process.env.ADMIRAL_AUTH_TOKEN,
-  path:           process.env.ADMIRAL_PATH,
-  hostname:       process.env.ADMIRAL_HOST,
+  authId: process.env.ADMIRAL_AUTH_ID,
+  authToken: process.env.ADMIRAL_AUTH_TOKEN,
+  path: process.env.ADMIRAL_PATH,
+  hostname: process.env.ADMIRAL_HOST,
 
-  buildId:        process.env.MAGELLAN_BUILD_ID,
-  buildName:      process.env.MAGELLAN_BUILD_NAME,
+  buildId: process.env.MAGELLAN_BUILD_ID,
+  buildName: process.env.MAGELLAN_BUILD_NAME,
 
-  buildURL:       process.env.BUILD_URL
+  buildURL: process.env.BUILD_URL
 };
