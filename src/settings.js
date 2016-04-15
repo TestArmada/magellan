9743d36d40cd2049fb2e530fe5f1dff8ab8f7de1
Maciej Adwent <maciekadwent@gmail.com>
2016-04-15 11:56:36 -0700

    Better temp dir logic

--- src/settings.js
+++ src/settings.js
@@ -17,13 +17,21 @@ var mkdirSync = require("./mkdir_sync");
 var TEMP_DIR = path.resolve(argv.temp_dir || "./temp");
 
 try {
+  // Check if TEMP_DIR already exists
+  fs.accessSync(TEMP_DIR, fs.R_OK | fs.W_OK);
+} catch (e) {
+  // Create it if it doesn't..
+  mkdirSync(TEMP_DIR);
+}
+
+try {
+  // Check if creation worked or if we have access to the directory we were told to use.
   fs.accessSync(TEMP_DIR, fs.R_OK | fs.W_OK);
   console.log("Magellan is creating temporary files at: " + TEMP_DIR);
 } catch (e) {
-  throw new Error("Magellan cannot write to the temporary directory: " + TEMP_DIR);
+  throw new Error("Magellan cannot write to or create the temporary directory: " + TEMP_DIR);
 }
 
-mkdirSync(TEMP_DIR);
 
 module.exports = {
 
