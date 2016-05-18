e3152d9dd8637a6da449246c44fbf31b990ccb86
Maciej Adwent <maciekadwent@gmail.com>
2016-05-18 16:21:55 -0700

    corrected std output

--- src/test_runner.js
+++ src/test_runner.js
@@ -405,21 +405,45 @@ TestRunner.prototype = {
     });
 
     childProcess.stdout.on("data", function (data) {
-      stdout += ("" + data)
-        .split("\n")
-        .map(function (line) {
-          return clc.greenBright(logStamp()) + " " + line;
-        })
-        .join("\n");
+      var text = ("" + data);
+      if (text.trim() !== "") {
+        text = text
+          .split("\n")
+          .filter(function (line) {
+            return line.trim() !== "" || line.indexOf("\n") > -1;
+          })
+          .map(function (line) {
+            return clc.greenBright(logStamp()) + " " + line;
+          })
+          .join("\n");
+
+        if (text.length > 0) {
+          stdout += text + "\n";
+        } else {
+          stdout += "\n";
+        }
+      }
     });
 
     childProcess.stderr.on("data", function (data) {
-      stderr += ("" + data)
-        .split("\n")
-        .map(function (line) {
-          return clc.greenBright(logStamp()) + " " + line;
-        })
-        .join("\n");
+      var text = ("" + data);
+      if (text.trim() !== "") {
+        text = text
+          .split("\n")
+          .filter(function (line) {
+            return line.trim() !== "" || line.indexOf("\n") > -1;
+          })
+          .map(function (line) {
+            return clc.greenBright(logStamp()) + " " + line;
+          })
+          .join("\n");
+
+        if (text.length > 0) {
+          stderr += text + "\n";
+        } else {
+          stderr += "\n";
+        }
+      }
     });
 
     childProcess.on("close", workerClosed);
