51d027741524968dfe88a75ca15c95b81f2a8da7
Maciej Adwent <maciekadwent@gmail.com>
2016-06-15 16:35:04 -0700

    Fix leaking of saucelabs tunnels and quiet down zombie killing output

--- src/util/process_cleanup.js
+++ src/util/process_cleanup.js
@@ -2,16 +2,19 @@
 
 var treeUtil = require("testarmada-tree-kill");
 var pid = process.pid;
+var settings = require("../settings");
 
 // Max time before we forcefully kill child processes left over after a suite run
 var ZOMBIE_POLLING_MAX_TIME = 15000;
 
 module.exports = function (callback) {
-  console.log("Checking for zombie processes...");
+  if (settings.debug) {
+    console.log("Checking for zombie processes...");
+  }
 
   treeUtil.getZombieChildren(pid, ZOMBIE_POLLING_MAX_TIME, function (zombieChildren) {
     if (zombieChildren.length > 0) {
-      console.log("Magellan giving up waiting for zombie child processes to die. Cleaning up..");
+      console.log("Giving up waiting for zombie child processes to die. Cleaning up..");
 
       var killNextZombie = function () {
         if (zombieChildren.length > 0) {
@@ -26,7 +29,9 @@ module.exports = function (callback) {
 
       return killNextZombie();
     } else {
-      console.log("No zombies found.");
+      if (settings.debug) {
+        console.log("No zombies found.");
+      }
       return callback();
     }
   });
