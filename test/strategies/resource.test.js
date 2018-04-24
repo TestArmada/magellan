66f302d6941d6fe5d335093f93649a726d7eb0b1
Lei Zhu <lzhu@walmart.com>
2018-04-24 10:47:10 -0700

    add test to resource strategy

--- test/strategies/resource.test.js
+++ test/strategies/resource.test.js
@@ -8,6 +8,11 @@ test('should construct with default rule', () => {
   expect(resource.name).toEqual('testarmada-magellan-no-resource-strategy');
 });
 
+test('should construct with given rule', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResource' });
+
+  expect(resource.name).toEqual('fake-resource-strategy');
+});
 
 test('should throw error if errors in module loading', () => {
   try {
@@ -23,27 +28,67 @@ test('should get description', () => {
   expect(resource.getDescription()).toEqual('Magellan doesn\'t require a resource manager to schedule test run');
 });
 
+test('should warn if no description', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResource' });
+  expect(resource.getDescription()).toEqual('');
+});
+
 test('should get failReason', () => {
   const resource = new Resource({});
   expect(resource.getFailReason()).toEqual('Magellan shouldn\'t depend on any resource manager to control test run');
 });
 
-test('should hold test resource', ()=>{
+test('should warn if no failReason', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResource' });
+  expect(resource.getFailReason()).toEqual('');
+});
+
+test('should hold test resource', () => {
   const resource = new Resource({});
   return expect(resource.holdTestResource('fake resource')).resolves.toEqual('fake resource');
 });
 
-test('should hold suite resources', ()=>{
+test('should return promise.resolve if no hold test resource', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResource' });
+  return expect(resource.holdTestResource('fake resource')).resolves.toEqual('fake resource');
+});
+
+test('should hold suite resources', () => {
   const resource = new Resource({});
   return expect(resource.holdSuiteResources('fake resources')).resolves.toEqual('fake resources');
 });
 
-test('should release test resource', ()=>{
+test('should return promise.resolve if no hold suite resources', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResource' });
+  return expect(resource.holdSuiteResources('fake resources')).resolves.toEqual('fake resources');
+});
+
+test('should release test resource', () => {
   const resource = new Resource({});
   return expect(resource.releaseTestResource('fake resource')).resolves.toEqual('fake resource');
 });
 
-test('should release suite resources', ()=>{
+test('should return promise.resolve if no release test resource', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResource' });
+  return expect(resource.releaseTestResource('fake resource')).resolves.toEqual('fake resource');
+});
+
+test('should return promise.resolve if release test resource fails', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResourceReject' });
+  return expect(resource.releaseTestResource('fake resource')).resolves.toEqual('fake resource');
+});
+
+test('should release suite resources', () => {
   const resource = new Resource({});
+  return expect(resource.releaseSuiteResources('fake resources')).resolves.toEqual('fake resources');
+});
+
+test('should return promise.resolve if no release suite resources', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResource' });
+  return expect(resource.releaseSuiteResources('fake resources')).resolves.toEqual('fake resources');
+});
+
+test('should return promise.resolve if release suite resources fails', () => {
+  const resource = new Resource({ strategy_resource: '../../test/strategies/mockResourceReject' });
   return expect(resource.releaseSuiteResources('fake resources')).resolves.toEqual('fake resources');
 });
