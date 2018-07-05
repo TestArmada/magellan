3f9a94174dc320c3e8b810c34a596431dd07b5d4
Lei Zhu <lzhu@walmart.com>
2018-07-05 13:49:37 -0700

    add more coverage

--- test/test_runner.test.js
+++ test/test_runner.test.js
@@ -52,7 +52,7 @@ describe("test_runner", () => {
         {
           name: "testarmada-magellan-local-executor",
           shortName: "local",
-          execute: () => {}
+          execute: () => { }
         }
       },
       listeners: [],
@@ -64,6 +64,7 @@ describe("test_runner", () => {
           description: "Magellan will bail as long as one test fails",
           bailReason: "At least one test has failed",
           shouldBail: () => { },
+          getBailReason: () => "FAKE_BAIL_REASON",
           decide: () => false
         },
         resource: {
@@ -79,7 +80,7 @@ describe("test_runner", () => {
       onFinish: () => Promise.resolve()
     }
   })
-  
+
   test("constructor", () => {
     const testRunner = initTestRunner(tests, options);
   });
@@ -109,7 +110,7 @@ describe("test_runner", () => {
         done();
       });
     });
-    
+
     test("test run exception results in test failure", (done) => {
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
@@ -121,7 +122,7 @@ describe("test_runner", () => {
         done();
       });
     });
-    
+
     test("test run error results in test failure", (done) => {
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
@@ -133,10 +134,10 @@ describe("test_runner", () => {
         done();
       });
     });
-    
+
     test("catch runtime resource error", (done) => {
       options.strategies.resource.holdTestResource = () => Promise.reject("FAILURE");
-      
+
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
 
@@ -145,12 +146,12 @@ describe("test_runner", () => {
         done();
       });
     });
-    
+
     test("worker error results in test failure", (done) => {
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
-      
-      testRunner.allocator.get = (cb) => cb(true, { index: 1});
+
+      testRunner.allocator.get = (cb) => cb(true, { index: 1 });
 
       testRunner.stageTestHandler(test, (error, test) => {
         expect(error).toBeDefined();
@@ -163,7 +164,7 @@ describe("test_runner", () => {
     test("successful test", (done) => {
       const testRunner = initTestRunner(tests, options);
       const error = jest.fn();
-      
+
       testRunner.completeTestHandler(error, {
         status: Test.TEST_STATUS_SUCCESSFUL,
         canRun: () => true,
@@ -176,7 +177,7 @@ describe("test_runner", () => {
       expect(error).not.toHaveBeenCalled();
       done();
     });
-    
+
     test("failed test", (done) => {
       options.strategies.bail.hasBailed = false;
       options.strategies.bail.shouldBail = function () { this.hasBailed = true; };
@@ -195,7 +196,7 @@ describe("test_runner", () => {
           done();
         });
     });
-    
+
     test("bailed test", (done) => {
       options.strategies.bail.hasBailed = true;
 
@@ -208,17 +209,17 @@ describe("test_runner", () => {
         workerIndex: 1,
         getRuntime: () => 3
       };
-      
+
       testRunner.completeTestHandler(null, test);
-      
+
       expect(test.status).toBe(Test.TEST_STATUS_SKIPPED)
       done();
     });
-    
+
     test("new test", (done) => {
       const testRunner = initTestRunner(tests, options);
       const error = jest.fn();
-      
+
       testRunner.completeTestHandler(error, {
         status: Test.TEST_STATUS_NEW,
         canRun: () => true,
@@ -231,12 +232,12 @@ describe("test_runner", () => {
       expect(error).not.toHaveBeenCalled();
       done();
     });
-    
+
     test("test that fail are enqueued again", (done) => {
       const testRunner = initTestRunner(tests, options);
       const error = jest.fn();
       const queueEnqueueSpy = jest.spyOn(testRunner.queue, "enqueue");
-      
+
       testRunner.completeTestHandler(error, {
         status: Test.TEST_STATUS_FAILED,
         canRun: () => false,
@@ -250,87 +251,87 @@ describe("test_runner", () => {
       done();
     });
   });
-  
+
   describe("completeQueueHandler", () => {
     beforeEach(() => {
       jest.useFakeTimers();
     });
-    
+
     test("listeners are successfully resolved", (done) => {
       const listener = new Reporter();
       listener.flush = () => Promise.resolve(10);
       const listenerFlushSpy = jest.spyOn(listener, "flush");
-      
-      options.listeners = [ listener ];
-      
+
+      options.listeners = [listener];
+
       const testRunner = initTestRunner(tests, options);
       testRunner.completeQueueHandler();
 
       jest.runAllTimers();
-      
+
       expect(setTimeout).toHaveBeenCalledTimes(1);
       expect(listenerFlushSpy).toHaveBeenCalled();
       done();
     });
-    
+
     test("listeners are successfully resolved even when flush has error", (done) => {
       const listener = new Reporter();
       listener.flush = () => Promise.reject();
       const listenerFlushSpy = jest.spyOn(listener, "flush");
-      
-      options.listeners = [ listener ];
-      
+
+      options.listeners = [listener];
+
       const testRunner = initTestRunner(tests, options);
       testRunner.completeQueueHandler();
 
       jest.runAllTimers();
-      
+
       expect(setTimeout).toHaveBeenCalledTimes(1);
       expect(listenerFlushSpy).toHaveBeenCalled();
       done();
     });
   });
-  
-  describe("run", () => {  
+
+  describe("run", () => {
     test("run is executed successfully", (done) => {
       const testRunner = initTestRunner(tests, options);
       testRunner.queue.proceed = jest.fn();
-      
+
       testRunner.run();
-      
+
       expect(testRunner.queue.proceed).toHaveBeenCalled();
       done();
     });
-    
+
     test("run in serial is executed successfully", (done) => {
       const testRunner = initTestRunner(tests, options);
       testRunner.serial = true;
       testRunner.queue.proceed = jest.fn();
-      
+
       testRunner.run();
-      
+
       expect(testRunner.queue.proceed).toHaveBeenCalled();
       done();
     });
   });
-  
-  describe("runTest", () => {    
+
+  describe("runTest", () => {
     beforeEach(() => {
       jest.useFakeTimers();
     });
-    
+
     test("test is run successfully", (done) => {
       fs.mkdirSync.mockImplementation((p) => { });
-      
+
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
-      
-      let worker = { 
-        index: 1, 
-        occupied: true, 
+
+      let worker = {
+        index: 1,
+        occupied: true,
         portOffset: 12000
       };
-      
+
       testRunner.execute = (testRun, test) => Promise.resolve({ error: false });
       const executeSpy = jest.spyOn(testRunner, "execute");
 
@@ -338,23 +339,23 @@ describe("test_runner", () => {
         expect(executeSpy).toHaveBeenCalledTimes(1);
         done();
       });
-      
+
       jest.runAllTimers();
     });
-    
+
     test("test in serial is run successfully", (done) => {
       fs.mkdirSync.mockImplementation((p) => { });
-      
+
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
-      
-      let worker = { 
-        index: 1, 
-        occupied: true, 
+
+      let worker = {
+        index: 1,
+        occupied: true,
         portOffset: 12000,
         token: "SOME_TOKEN"
       };
-      
+
       testRunner.execute = (testRun, test) => Promise.resolve({ error: false });
       const executeSpy = jest.spyOn(testRunner, "execute");
 
@@ -363,23 +364,23 @@ describe("test_runner", () => {
         expect(executeSpy).toHaveBeenCalledTimes(1);
         done();
       });
-      
+
       jest.runAllTimers();
     });
-    
+
     test("failed test is rejected", (done) => {
       fs.mkdirSync.mockImplementation((p) => { });
-      
+
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
-      
-      let worker = { 
-        index: 1, 
-        occupied: true, 
+
+      let worker = {
+        index: 1,
+        occupied: true,
         portOffset: 12000,
         token: "SOME_TOKEN"
       };
-      
+
       testRunner.execute = (testRun, test) => Promise.reject("EXECUTION_ERROR");
       const executeSpy = jest.spyOn(testRunner, "execute");
 
@@ -387,43 +388,43 @@ describe("test_runner", () => {
         expect(executeSpy).toHaveBeenCalledTimes(1);
         done();
       });
-      
+
       jest.runAllTimers();
     });
-    
+
     test("empty buildId is rejected", (done) => {
       fs.mkdirSync.mockImplementation((p) => { });
 
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
-      
-      let worker = { 
-        index: 1, 
-        occupied: true, 
+
+      let worker = {
+        index: 1,
+        occupied: true,
         portOffset: 12000,
         token: "SOME_TOKEN"
       };
-      
+
       testRunner.buildId = null;
 
       testRunner.runTest(test, worker).catch((v) => {
         done();
       });
-      
+
       jest.runAllTimers();
     });
   });
-  
-  describe("execute", () => {  
+
+  describe("execute", () => {
     beforeEach(() => {
       jest.useFakeTimers();
     });
-    
+
     test("test is executed successfully", (done) => {
       const listener = new Reporter();
       const listenerListenToSpy = jest.spyOn(listener, "listenTo");
 
-      options.listeners = [ listener ];
+      options.listeners = [listener];
 
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
@@ -435,19 +436,63 @@ describe("test_runner", () => {
       testRunner.execute(testRun, test);
       jest.runOnlyPendingTimers();
       jest.runOnlyPendingTimers();
-      
+
       expect(listenerListenToSpy).toHaveBeenCalled();
       done();
     });
-    
+
+    test("enable executor", (done) => {
+      const listener = new Reporter();
+      const listenerListenToSpy = jest.spyOn(listener, "listenTo");
+
+      options.listeners = [listener];
+
+      const testRunner = initTestRunner(tests, options);
+      testRunner.queue.isIdle = () => false;
+      const test = stubPassTest();
+      const testRun = {
+        enableExecutor: (executor) => { },
+        guid: "",
+        getEnvironment: (opts) => ""
+      };
+
+      testRunner.execute(testRun, test);
+      jest.runOnlyPendingTimers();
+      jest.runOnlyPendingTimers();
+
+      expect(listenerListenToSpy).toHaveBeenCalled();
+      done();
+    });
+
+    test("failed test with time out", (done) => {
+      const listener = new Reporter();
+      const listenerListenToSpy = jest.spyOn(listener, "listenTo");
+
+      options.listeners = [listener];
+
+      const testRunner = initTestRunner(tests, options);
+      const test = stubFailTest();
+      const testRun = {
+        guid: "",
+        getEnvironment: (opts) => ""
+      };
+
+      testRunner.execute(testRun, test);
+      jest.runOnlyPendingTimers();
+      jest.runOnlyPendingTimers();
+
+      expect(listenerListenToSpy).toHaveBeenCalled();
+      done();
+    });
+
     test("bailing a test closes the execution worker", (done) => {
       const listener = new Reporter();
       const listenerListenToSpy = jest.spyOn(listener, "listenTo");
 
-      options.listeners = [ listener ];
+      options.listeners = [listener];
       options.strategies.bail.hasBailed = true;
       options.strategies.bail.getBailReason = jest.fn();
-      
+
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
       const testRun = {
@@ -458,18 +503,18 @@ describe("test_runner", () => {
       testRunner.execute(testRun, test);
       jest.runOnlyPendingTimers();
       jest.runOnlyPendingTimers();
-      
+
       expect(listenerListenToSpy).toHaveBeenCalled();
       expect(options.strategies.bail.getBailReason).toHaveBeenCalled();
       done();
     });
-    
+
     test("test environment error results in promise rejection", () => {
       const listener = new Reporter();
       const listenerListenToSpy = jest.spyOn(listener, "listenTo");
 
-      options.listeners = [ listener ];
-      
+      options.listeners = [listener];
+
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
       const testRun = {
@@ -481,12 +526,12 @@ describe("test_runner", () => {
 
       return testRunner.execute(testRun, test).catch(e => expect(e).toMatch("ENVIRONMENT_ERROR"));
     });
-    
+
     test("child process error results in promise rejection", () => {
       const listener = new Reporter();
       const listenerListenToSpy = jest.spyOn(listener, "listenTo");
 
-      options.listeners = [ listener ];
+      options.listeners = [listener];
       options.executors.local.execute = () => {
         throw "CHILD_PROCESS_ERROR";
       };
@@ -500,14 +545,14 @@ describe("test_runner", () => {
 
       return testRunner.execute(testRun, test).catch(e => expect(e).toMatch("CHILD_PROCESS_ERROR"));
     });
-    
+
     test("listener error results in promise rejection", () => {
       const listener = new Reporter();
       listener.listenTo = () => {
         throw "LISTENER_ERROR";
       };
 
-      options.listeners = [ listener ];
+      options.listeners = [listener];
 
       const testRunner = initTestRunner(tests, options);
       const test = stubPassTest();
@@ -519,56 +564,105 @@ describe("test_runner", () => {
       return testRunner.execute(testRun, test).catch(e => expect(e).toMatch("LISTENER_ERROR"));
     });
   });
-  
-  describe("logTestsSummary", () => {    
+
+  describe("logTestsSummary", () => {
     test("empty test print no warning", (done) => {
       const warnSpy = jest.spyOn(logger, "warn");
 
       const testRunner = initTestRunner(tests, options);
 
       testRunner.logTestsSummary();
-      
+
       expect(warnSpy).not.toHaveBeenCalled();
       done();
     });
-    
+
     test("passing 1 test and failing 1 test prints 4 warnings", (done) => {
       const loggerLogSpy = jest.spyOn(logger, "log");
       const loggerWarnSpy = jest.spyOn(logger, "warn");
       const analyticsMarkSpy = jest.spyOn(analytics, "mark");
-      
+
       const testRunner = initTestRunner(tests, options);
       enqueuePassedTest(testRunner);
       enqueueFailedTest(testRunner);
 
       testRunner.logTestsSummary();
-      
+
       expect(loggerLogSpy).toHaveBeenCalled();
       expect(loggerWarnSpy).toHaveBeenCalled();
       expect(analyticsMarkSpy).toHaveBeenCalledWith("magellan-run", "failed");
       done();
     });
-    
+
     test("failing 1 test with bail prints 4 warnings with bail reason", (done) => {
       options.strategies.bail.hasBailed = true;
       options.strategies.bail.getBailReason = () => "Some bail reason";
-      
+
       const loggerLogSpy = jest.spyOn(logger, "log");
       const loggerWarnSpy = jest.spyOn(logger, "warn");
       const analyticsMarkSpy = jest.spyOn(analytics, "mark");
       const bailReasonSpy = jest.spyOn(options.strategies.bail, "getBailReason");
 
       const testRunner = initTestRunner(tests, options);
-      enqueueFailedTest(testRunner);
+
+      testRunner.queue = new TestQueue({
+        tests: [{
+          status: Test.TEST_STATUS_SUCCESSFUL,
+          getRetries: () => 2,
+        }, {
+          status: Test.TEST_STATUS_FAILED,
+          getRetries: () => 2,
+        }, {
+          status: Test.TEST_STATUS_NEW,
+          getRetries: () => 0,
+        }],
+        getTestAmount: () => 3,
+        getPassedTests: () => 1,
+        getFailedTests: () => 1,
+        workerAmount: 1,
+        completeQueueHandler: () => Promise.resolve(1),
+        stageTestHandler: (test, cb) => cb()
+      });
 
       testRunner.logTestsSummary();
-      
+
       expect(loggerLogSpy).toHaveBeenCalled();
       expect(loggerWarnSpy).toHaveBeenCalled();
       expect(bailReasonSpy).toHaveBeenCalled();
       expect(analyticsMarkSpy).toHaveBeenCalledWith("magellan-run", "failed");
       done();
     });
+
+  });
+
+  test("gather trends", () => {
+    const loggerLogSpy = jest.spyOn(logger, "log");
+    const testRunner = initTestRunner(tests, options);
+
+    jest.mock("fs", () => {
+      return {
+        readFileSync: () => {
+          console.log("some fake call")
+          return {
+            failures: {
+              "FAKE_FAILURE": 2,
+              "ANOTHER_FAILURE": 1
+            }
+          }
+        },
+        writeFileSync: () => { }
+      }
+    });
+
+    testRunner.trends = {
+      failures: {
+        "FAKE_FAILURE": 1
+      }
+    };
+
+    testRunner.gatherTrends();
+
+    expect(loggerLogSpy).toHaveBeenCalled();
   });
 });
 
@@ -584,13 +678,13 @@ function initTestRunner(tests, options, numPassedTests, numFailedTests) {
           a: () => true,
           b: () => true
         },
-        TestRun: class {}
+        TestRun: class { }
       },
       BASE_PORT_SPACING: 3
     },
     startTime: (new Date()).getTime()
   });
-  
+
   testRunner.queue = new TestQueue({
     tests: [],
     workerAmount: 1,
@@ -604,33 +698,34 @@ function initTestRunner(tests, options, numPassedTests, numFailedTests) {
 function enqueuePassedTest(testRunner) {
   testRunner.queue.tests.push({
     status: Test.TEST_STATUS_SUCCESSFUL,
-    getRetries: () => true
+    getRetries: () => 0
   });
 }
 
 function enqueueFailedTest(testRunner) {
   testRunner.queue.tests.push({
     status: Test.TEST_STATUS_FAILED,
-    getRetries: () => true
+    getRetries: () => 2
   });
 }
 
 function stubPassTest() {
   return {
     executor: {
+      shortName: "FAKE_EXECUTOR_SHORT_NAME",
       setupTest: (cb) => cb(null, "FAKE_TOKEN"),
       teardownTest: (token, cb) => cb(),
-      summerizeTest: () => {},
+      summerizeTest: () => { },
       getPorts: jest.fn()
     },
     profile: {
       executor: "local"
     },
     locator: "",
-    startClock: () => {},
-    stopClock: () => {},
+    startClock: () => { },
+    stopClock: () => { },
     getRuntime: () => 0,
-    stdout: () => {},
+    stdout: () => { },
     pass: jest.fn(),
     fail: jest.fn()
   }
@@ -641,17 +736,17 @@ function stubFailTest() {
     executor: {
       setupTest: (cb) => cb("FAKE_ERROR", "FAKE_TOKEN"),
       teardownTest: (token, cb) => cb(),
-      summerizeTest: () => {},
+      summerizeTest: (buildId, metadata, cb) => cb(),
       getPorts: jest.fn()
     },
     profile: {
       executor: "local"
     },
     locator: "",
-    startClock: () => {},
-    stopClock: () => {},
-    getRuntime: () => 0,
-    stdout: () => {},
+    startClock: () => { },
+    stopClock: () => { },
+    getRuntime: () => 1000000000,
+    stdout: () => { },
     pass: jest.fn(),
     fail: jest.fn()
   }
