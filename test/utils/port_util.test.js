307dd6c1fce167e8b921682c769ebda63a01053a
cahrens <cahrens@walmartlabs.com>
2020-04-24 09:56:43 -0700

    Updated logic to check every port in the range, and move to the next set

--- test/utils/port_util.test.js
+++ test/utils/port_util.test.js
@@ -59,3 +59,44 @@ test('should throw exception after maximum retries', () => {
   expect(spy.called).toEqual(true);
   expect(spy.args[0][0].message).toEqual('Gave up looking for an available port after 100 attempts.');
 });
+
+test('should acquire next port if any port is not available', () => {
+
+  // can't reset port_util.portCursor, so we just have to start where it left off
+  expect(portUtil.getNextPort()).toEqual(12327)
+
+  checkPorts.mockImplementation((arr, cb) => {
+    // second port not available
+    arr[1] === 12331 ? cb([{
+      port: arr[0],
+      available: false
+    }]) : cb([{
+      port: arr[0],
+      available: true
+    }]);
+  });
+
+  const spy = sinon.spy();
+  portUtil.acquirePort(spy);
+  expect(spy.called).toEqual(true);
+  expect(spy.args[0]).toEqual([null, 12333]);
+
+  // can't reset port_util.portCursor, so we just have to start where it left off
+  expect(portUtil.getNextPort()).toEqual(12336)
+
+  checkPorts.mockImplementation((arr, cb) => {
+    // third port not available
+    arr[1] === 12340 ? cb([{
+      port: arr[0],
+      available: false
+    }]) : cb([{
+      port: arr[0],
+      available: true
+    }]);
+  });
+
+  const spy2 = sinon.spy();
+  portUtil.acquirePort(spy2);
+  expect(spy2.called).toEqual(true);
+  expect(spy2.args[0]).toEqual([null, 12342]);
+});
